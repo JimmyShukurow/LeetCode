45b31edbc9caf581453c806e712dc26b44402e5b
Your Name <you@example.com>
2024-07-15 22:29:26 +0300

    medium leetcode80 not solved yet

--- src/main/java/io/smartir/leetcode/DSA/medium/LeetCode80.java
+++ src/main/java/io/smartir/leetcode/DSA/medium/LeetCode80.java
@@ -4,16 +4,66 @@ import java.util.Arrays;
 
 public class LeetCode80 {
     public int removeDuplicates(int[] nums) {
-        int counter = 0;
+        nums = updateArray(nums);
+        return calculateCounter(nums);
+    }
+
+    public int calculateCounter(int[] nums) {
+        int counter = 1;
+        if (nums[0]==nums[1]) counter++;
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                counter++;
+            } else if (i>0 && i< nums.length-1 ) {
+                if (nums[i-1] != nums[i] && nums[i] == nums[i+1]) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public int[] updateArray(int[] nums) {
+        int innerCounter = 0;
         int slowPointer = 0;
         int fastPointer = 0;
+        int k = 1;
+        while (slowPointer < nums.length - 1) {
+            if (nums[slowPointer] == nums[slowPointer + 1] && innerCounter < 1) {
+                slowPointer++;
+                innerCounter++;
+                continue;
+            }
+            if (nums[slowPointer] != nums[slowPointer + 1] && innerCounter < 1) {
+                slowPointer++;
+                innerCounter = 0;
+                continue;
+            }
+            if (nums[slowPointer] == nums[slowPointer + 1] && innerCounter > k) {
+                fastPointer = slowPointer;
+                while (nums[slowPointer] == nums[fastPointer]) {
+                    if (fastPointer >= nums.length - 1) break;
+                    fastPointer++;
 
-        while (slowPointer < nums.length) {
+                }
 
+                int temp = slowPointer + 1;
+                while (temp < fastPointer) {
+                    nums[temp] = nums[fastPointer];
+                    temp++;
+                }
+                if (fastPointer < nums.length - 1 && nums[fastPointer] == nums[fastPointer + 1]) {
+                    nums[++slowPointer] = nums[fastPointer];
+                } else {
+                    k = 0;
+                }
+                slowPointer++;
+                innerCounter = 0;
+            }
+            innerCounter++;
         }
 
         System.out.println(Arrays.toString(nums));
-
-        return counter;
+        return nums;
     }
 }
--- src/test/java/io/smartir/leetcode/DSA/medium/LeetCode80Test.java
+++ src/test/java/io/smartir/leetcode/DSA/medium/LeetCode80Test.java
@@ -11,10 +11,30 @@ class LeetCode80Test {
     public void removeDuplicates() {
         //given
         LeetCode80 leetCode80 = new LeetCode80();
-        int[] given = {1, 1, 1, 2, 2, 3};
+        int[] given = {1,1,1,2,2,3};
         //when
         var result = leetCode80.removeDuplicates(given);
         //then
-        assertThat(result).isEqualTo(new int[]{1, 1, 2, 2, 3, 3});
+        assertThat(result).isEqualTo(5);
+    }
+    @Test
+    public void removeDuplicates2() {
+        //given
+        LeetCode80 leetCode80 = new LeetCode80();
+        int[] given = {0,0,1,1,1,1,2,3,3};
+        //when
+        var result = leetCode80.removeDuplicates(given);
+        //then
+        assertThat(result).isEqualTo(7);
+    }
+    @Test
+    public void removeDuplicates3() {
+        //given
+        LeetCode80 leetCode80 = new LeetCode80();
+        int[] given = {0,0,1,1,1,1,1,1,1,1,1,2,3,3};
+        //when
+        var result = leetCode80.removeDuplicates(given);
+        //then
+        assertThat(result).isEqualTo(7);
     }
 }
